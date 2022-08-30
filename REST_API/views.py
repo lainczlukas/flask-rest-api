a939670d8ee880b23a3f775f0b65b2cd06cc5f9d
Lukáš Laincz <lukas.laincz@gmail.com>
2022-08-30 22:06:35 +0200

    css

--- REST_API/views.py
+++ REST_API/views.py
@@ -3,13 +3,8 @@ from REST_API.models import Article
 from flask import render_template, request, jsonify, make_response
 
 
-@app.route("/")
+@app.route("/", methods=('GET', 'POST'))
 def home():
-    return render_template("home.html")
-
-
-@app.route("/form", methods=('GET', 'POST'))
-def form():
     if request.method == 'POST':
             req = request.get_json()
 
@@ -17,4 +12,4 @@ def form():
 
             return make_response(jsonify({'message': 'json received'}), 200)
     else:
-        return render_template("form.html")
+        return render_template("home.html")
