000d37769e6c1bd1ad61b62e65e1f6d4157bb822
Lukáš Laincz <lukas.laincz@gmail.com>
2022-09-01 00:03:26 +0200

    get method

--- REST_API/views.py
+++ REST_API/views.py
@@ -3,13 +3,19 @@ from REST_API.models import Article
 from flask import render_template, request, jsonify, make_response
 
 
-@app.route("/", methods=('GET', 'POST'))
+@app.route("/", methods=['GET'])
 def home():
+    return render_template("home.html")
+
+@app.route("/form", methods=['POST'])
+def form():
     if request.method == 'POST':
             req = request.get_json()
 
             print(req)
 
             return make_response(jsonify({'message': 'json received'}), 200)
-    else:
-        return render_template("home.html")
+
+@app.route("/form/<int:id>", methods=['GET'])
+def get_article(id):
+    return make_response(jsonify({'message': 'article id is {}'.format(id)}), 200)
