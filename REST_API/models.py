58be3153c96ccbe1648565fb7797c6691855442d
Lukáš Laincz <lukas.laincz@gmail.com>
2022-09-02 23:07:12 +0200

    database

--- REST_API/models.py
+++ REST_API/models.py
@@ -2,8 +2,8 @@ from REST_API import db
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50))
-    body = db.Column(db.String(150))
+    title = db.Column(db.String(50), nullable=False)
+    body = db.Column(db.String(150), nullable=False)
 
     def __repr__(self):
         return f"Article title: {self.title}, body: {self.body}"
--- REST_API/views.py
+++ REST_API/views.py
@@ -1,4 +1,4 @@
-from REST_API import app
+from REST_API import app, db
 from REST_API.models import Article
 from flask import render_template, request, jsonify, make_response
 
@@ -7,15 +7,32 @@ from flask import render_template, request, jsonify, make_response
 def home():
     return render_template("home.html")
 
+
 @app.route("/form", methods=['POST'])
 def form():
     if request.method == 'POST':
             req = request.get_json()
 
-            print(req)
+            title = req['title']
+            body = req['body']
+            article = Article(title=title, body=body)
+            db.session.add(article)
+            db.session.commit()
 
-            return make_response(jsonify({'message': 'json received'}), 200)
+            return make_response(jsonify({'message': get_articles()}), 200)
+
 
 @app.route("/form/<int:id>", methods=['GET'])
 def get_article(id):
     return make_response(jsonify({'message': 'article id is {}'.format(id)}), 200)
+
+
+def get_articles():
+    articles = Article.query.all()
+
+    data = []
+
+    for article in articles:
+        data.append(Article(title=article.title, body=article.body))
+
+    return data
